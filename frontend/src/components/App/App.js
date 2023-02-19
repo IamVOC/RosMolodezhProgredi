8863f496e21fbe6bfdab1ef3695935d9c23602bf
Egor <egor951769794@gmail.com>
2023-02-19 10:16:55 +0600

    something

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -8,7 +8,7 @@ export default function App() {
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/main" element={<MainPage />} />
+                    <Route path="/" element={<MainPage />} />
                     <Route path="/dash" element={<DashPage />} />
                 </Routes>
             </BrowserRouter>
